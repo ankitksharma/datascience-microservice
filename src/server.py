4031bd92265ce4a3cd73c20c018cd27d0c4a1e81
Ankit Sharma <ankit.sharma@target.com>
2018-05-13 16:38:16 +0530

    Basic project structure and server code

---
+++ src/server.py
@@ -0,0 +1,60 @@
+import falcon
+import json
+from logger import logging
+logger = logging.getLogger(__name__)
+
+
+def make_response(res, data, status):
+    res.body = json.dumps(data, indent=2, ensure_ascii=False)
+    res.status = status
+    res.append_header('Access-Control-Allow-Origin', '*')
+
+
+class StaticResource(object):
+    @classmethod
+    def on_get(cls, req, resp):
+        resp.status = falcon.HTTP_200
+        resp.content_type = 'text/html'
+        resp.body = "<h1 style='color:red'>Data Science MicroService</h1>"
+
+
+class UpdateResource(object):
+    @classmethod
+    def on_get(cls, req, resp):
+        """Handles GET requests"""
+        # Return note for particular ID
+        if req.get_param("id"):
+            resp.body = json.dumps({"result": "we got:" + req.get_param("id")})
+            make_response(resp, {"result": "SUCCESS"}, falcon.HTTP_200)
+        else:
+            resp.body = "Service to host data science project"
+
+    @classmethod
+    def on_post(cls, req, resp):
+        """Handles POST requests"""
+        try:
+            raw_json = req.stream.read()
+        except Exception as ex:
+            raise falcon.HTTPError(falcon.HTTP_400, 'Error', ex.message)
+
+        try:
+            result = json.loads(raw_json, encoding='utf-8')
+            logger.info(result['text'])
+            resp.body = json.dumps({"result": 'Successfully inserted' + result['text']})
+        except ValueError:
+            raise falcon.HTTPError(falcon.HTTP_400, 'Invalid JSON',
+                                   'Could not decode the request body. The ''JSON was incorrect.')
+
+
+def handle_404(req, resp):
+    data = {
+        'status': '404 - Not Found'
+    }
+    status = falcon.HTTP_404
+    make_response(resp, data, status)
+
+
+api = falcon.API()
+api.add_route('/', StaticResource())
+api.add_route('/update', UpdateResource())
+api.add_sink(handle_404, '')
